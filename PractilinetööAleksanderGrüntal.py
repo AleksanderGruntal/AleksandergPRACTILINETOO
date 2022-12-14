eb8c445eb85d45b16b7639fd77cf502003afe891
opilane <opilane@HP-CZC6297BB4>
2022-12-14 15:38:49 +0200

    Add project files.

---
+++ PractilinetööAleksanderGrüntal.py
@@ -0,0 +1,32 @@
+﻿#1
+from math import *
+from random import *
+n=float(input("введенное число"))
+try:
+   a=int(input("Sisesta arv"))
+   if a>0:
+        print(f"Positiivne")
+      if a%2==0:
+         print(f"{a} on paaritus")
+      else:
+         print(f"{a} on paaritu")
+   else:
+       print("Negatiivne")
+except:
+    print("Vale andmetüüp")
+
+#2
+from math import *
+a=float(input("напишите число => *"))
+b=float(input("напишите число => *"))
+v=float(input("напишите число => *"))
+try:
+    if(a,b,c)>0:
+        print()
+    if a,b,c
+
+
+
+
+
+
